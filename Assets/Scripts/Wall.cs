9bd7905dac2531cb22f17c3470c71026ce271b61
Marin Aussant <118370229+MarinAussant@users.noreply.github.com>
2024-01-24 18:20:52 +0100

    But Anim

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
 public class Wall : MonoBehaviour
@@ -11,28 +12,128 @@ public class Wall : MonoBehaviour
     [SerializeField] private GameObject barExtendTile;
     [SerializeField] private GameObject stunTile;
 
+    [SerializeField] private GameObject goalP1Particle;
+    [SerializeField] private GameObject goalP2Particle;
+
+    [SerializeField] private GameObject resetP1Particle;
+    [SerializeField] private GameObject resetP2Particle;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Ball")
         {
             Destroy(collision.gameObject);
-            ResetTerrain();
-            ResetPlayers();
 
             GameObject tempBall = Instantiate(ball, new Vector2(-0.06f, -0.6f), transform.rotation);
 
             if (isPlayerOne)
             {
                 GameObject.Find("Player2").GetComponent<PlayerTwo>().AddScore();
-                tempBall.GetComponent<Ball>().BallInit(true);
+                StartCoroutine(AnimationP2E1());
             }
             else
             {
                 GameObject.Find("Player1").GetComponent<PlayerOne>().AddScore();
-                tempBall.GetComponent<Ball>().BallInit(false);
+                StartCoroutine(AnimationP1E1());
             }
 
         }
+    }
+
+    IEnumerator AnimationP1E1()
+    {
+
+        Instantiate(goalP1Particle);
+
+        yield return new WaitForSeconds(1);
+
+        StartCoroutine("AnimationP1E2");
+
+    }
+    IEnumerator AnimationP1E2()
+    {
+
+        Instantiate(resetP2Particle);
+
+        yield return new WaitForSeconds(0.5f);
+
+        StartCoroutine("AnimationP1E3");
+
+    }
+    IEnumerator AnimationP1E3()
+    {
+
+        ResetTerrain();
+        ResetPlayers();
+
+        yield return new WaitForSeconds(1);
+
+        FindAnyObjectByType<Ball>().BallInit(false);
+    }
+
+    IEnumerator AnimationP2E1()
+    {
+
+        Instantiate(goalP2Particle);
+
+        yield return new WaitForSeconds(1);
+
+        StartCoroutine("AnimationP2E2");
+
+    }
+    IEnumerator AnimationP2E2()
+    {
+
+        Instantiate(resetP1Particle);
+
+        yield return new WaitForSeconds(0.5f);
+
+        StartCoroutine("AnimationP2E3");
+
+    }
+    IEnumerator AnimationP2E3()
+    {
+
+        ResetTerrain();
+        ResetPlayers();
+
+        yield return new WaitForSeconds(1);
+
+        FindAnyObjectByType<Ball>().BallInit(true);
+    }
+
+
+
+    public void AnimationP2()
+    {
+        float timeStamp = 0f;
+        Instantiate(goalP2Particle);
+
+        while (timeStamp < 1)
+        {
+            timeStamp += Time.deltaTime;
+        }
+
+        Instantiate(resetP1Particle);
+        timeStamp = 0f;
+
+        while (timeStamp < 0.3)
+        {
+            timeStamp += Time.deltaTime;
+        }
+
+        ResetTerrain();
+        ResetPlayers();
+
+        timeStamp = 0f;
+
+        while (timeStamp < 1)
+        {
+            timeStamp += Time.deltaTime;
+        }
+
+        FindAnyObjectByType<Ball>().BallInit(true);
+
     }
 
     public void ResetTerrain()
